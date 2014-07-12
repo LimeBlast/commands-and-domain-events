36c7c2245f3e2ffe67b1fcad2856f3ceedcf0eca
Daniel Hollands <daniel@limeblast.co.uk>
2014-07-12 11:45:57 +0100

    Updated the HTML a little.

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Commands and Domain Events</title>
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>
 	<link rel="stylesheet" href="/css/main.css"/>
 	<style>
