7e79075408b0eb5bd5d252275026315b798a665a
Daniel Hollands <daniel@limeblast.co.uk>
2014-07-12 10:05:38 +0100

    Added an event listener.. but we're about to make it extra awesome, so I wanted to commit here so I can do a diff between the two implementations.

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,6 +1,18 @@
 <?php
 
+use Acme\Commanding\CommandBus;
+
 class BaseController extends Controller {
+
+	/**
+	 * @var Acme\Commanding\CommandBus
+	 */
+	protected $commandBus;
+
+	function __construct(CommandBus $commandBus)
+	{
+		$this->commandBus = $commandBus;
+	}
 
 	/**
 	 * Setup the layout used by the controller.
@@ -9,8 +21,7 @@ class BaseController extends Controller {
 	 */
 	protected function setupLayout()
 	{
-		if ( ! is_null($this->layout))
-		{
+		if (! is_null($this->layout)) {
 			$this->layout = View::make($this->layout);
 		}
 	}
--- app/controllers/JobsController.php
+++ app/controllers/JobsController.php
@@ -1,19 +1,8 @@
 <?php
 
-use Acme\Commanding\CommandBus;
 use Acme\Jobs\PostJobListingCommand;
 
 class JobsController extends BaseController {
-
-	/**
-	 * @var Acme\Commanding\CommandBus
-	 */
-	private $commandBus;
-
-	function __construct(CommandBus $commandBus)
-	{
-		$this->commandBus = $commandBus;
-	}
 
 	public function create()
 	{
@@ -27,13 +16,6 @@ class JobsController extends BaseController {
 	 */
 	public function store()
 	{
-		// validate form input
-		// if not valid, go back
-		// otherwise, create job record in db
-		// send user confirmation
-		// notify all listeners of a particular job tag
-		// etc.
-
 		$input = Input::only('title', 'description');
 
 		$command = new PostJobListingCommand($input['title'], $input['description']);
